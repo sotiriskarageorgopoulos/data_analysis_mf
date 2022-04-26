3b8758da963c1b7c77893e230e91dcf10327aba5
sotiriskarageorgopoulos <sotiriskarageorgopoulos@gmail.com>
2022-04-26 17:35:20 +0300

    exercise 1 and 3 is ready!

--- R/scripts/exercise_1.R
+++ R/scripts/exercise_1.R
@@ -1,1 +1,11 @@
-kyphosis <- read.csv("../datasets/")#kypshosis data set is missing
+library('rpart')
+data("kyphosis")
+kyph <- kyphosis
+boxplot(kyph$Number)
+outliers <- boxplot.stats(kyphosis$Number)$out
+print(outliers)
+outliers_indexes <- which(kyphosis$Number %in% c(outliers))
+print(outliers_indexes) #outliers indexes from boxplot
+
+#identify outliers on the boxplot
+identify(rep(1, length(outliers)),outliers,labels = seq_along(outliers))
--- R/scripts/exercise_3.R
+++ R/scripts/exercise_3.R
@@ -1,1 +1,6 @@
-#mtcars dataset is missing.
+mtcars <- read.table("C:\\Users\\sotir\\Dev\\data_analysis_mf\\datasets\\mtcars.csv",sep=",",header=TRUE)
+automatic_transmission <- mtcars[(mtcars$am == 0),]$mpg
+manual_transmission <- mtcars[(mtcars$am == 1),]$mpg
+
+#The difference is 24.39231-17.14737=7.24494. There is a mean mpg difference 7.24494 between cars with automatic transmissions and cars with manual transmissions.
+print(t.test(automatic_transmission,manual_transmission,conf.level=0.95))
