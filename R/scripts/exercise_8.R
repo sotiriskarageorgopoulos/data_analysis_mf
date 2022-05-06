9fefe6f39a1b671a70a6ca28df641f3ee0f28005
Konstantinos Koutsoukos <ko.koutsoukos@gmail.com>
2022-05-06 12:39:14 +0300

    exercise 8 update

--- R/scripts/exercise_8.R
+++ R/scripts/exercise_8.R
@@ -1,2 +1,34 @@
 library(readxl)
 market <- read_excel("C:\\Users\\Kostas\\Dev\\data_analysis_mf\\datasets\\market.xlsx")
+
+#i
+model <- lm(Sales ~ Preis + Costs + Arrivals , data = market)
+summary(model)
+
+summary(model)$coefficient
+
+#ii
+modelCosts <- lm(Sales ~ Preis, data = market)
+summary(modelCosts)
+
+modelArrivals <- lm(Sales ~ Arrivals, data = market)
+summary(modelArrivals)
+
+modelPrice <- lm(Sales ~ Preis, data = market)
+summary(modelPrice)
+
+#iii
+cordata <- market[c("Sales","Preis","Costs","Arrivals")]
+corr <- round(cor(cordata), 1)
+
+ggcorrplot(corr, hc.order = TRUE, type = "lower", lab = TRUE, lab_size = 4,
+           method="circle", colors = c("blue", "white", "red"), outline.color = "gray",
+           show.legend = TRUE, show.diag = FALSE, title="Correlogram of Sales variables")
+
+#iv
+standard_res <- rstandard(model)
+summary(standard_res)
+
+plot(market$Sales, standard_res, xlab='Sales', ylab='Standardized Residuals')
+#add horizontal line at 0
+abline(0, 0)
